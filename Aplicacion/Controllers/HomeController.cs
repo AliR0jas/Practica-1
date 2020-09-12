6cab5fba7107f7fb92e57bac994f66a6f7f161a4
AliRojas <171G0475@rcarbonifera.tecnm.mx>
2020-09-11 23:42:17 -0500

    version 1

---
+++ Aplicacion/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Resultado(Practica p)
+        {
+            return View(p);
+        }
+    }
+}
---
+++ Aplicacion/Models/Practica.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Models
+{
+    public class Practica
+    {
+        public int NumeroUno { get; set; }
+        public int NumeroDos { get; set; }
+        public int Res()
+        {
+            return NumeroUno + NumeroDos;
+        }
+    }
+}
